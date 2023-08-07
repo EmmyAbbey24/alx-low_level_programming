866306db3ccec6ceaed085216bdd23d782bdadd4
EmmyAbbey24 <princeemmy010@gmail.com>
2023-08-07 05:01:43 -0700

    static library

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letters
+ * @o: char to check
+ *
+ * Return: if upcase is 1, else return 0
+ */
+
+int _isupper(int o)
+
+{
+if (o >= 'A' && o <= 'Z')
+
+return (1);
+
+else
+
+return (0);
+
+}
