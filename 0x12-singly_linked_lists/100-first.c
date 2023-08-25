88916032f56de4ff17d102ff7c6001be669e9bc4
EmmyAbbey24 <princeemmy010@gmail.com>
2023-08-25 16:38:14 -0700

    string fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,27 +1,14 @@
 #include <stdio.h>
 
-/* Declare the function prototype */
-void print_intro(void) __attribute__((constructor));
+void first(void) __attribute__ ((constructor));
 
 /**
- * print_intro - Prints an introduction
- *
- * Description: This function is executed automatically
- * before the main function is called. It prints two lines.
+ * first - prints a sentence before the main
+ * function is executed
  */
-void print_intro(void)
+void first(void)
 {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-    /* The main function is empty in this example */
-    return 0;
-}
