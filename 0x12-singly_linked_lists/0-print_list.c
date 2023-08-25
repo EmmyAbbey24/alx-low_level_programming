fc0a6535c10ce1e6a4a99f19d0bb0f3da0aa122c
EmmyAbbey24 <princeemmy010@gmail.com>
2023-08-25 13:59:08 -0700

    string commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,15 +8,22 @@
  * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
-
 {
 size_t count = 0;
-while (!h = NULL)
-if (h->str == NULL
+
+while (h != NULL)
+{
+if (h->str == NULL)
+{
 printf("[0] (nil)\n");
+}
 else
+{
 printf("[%u] %s\n", h->len, h->str);
+}
 h = h->next;
 count++;
+}
+
 return (count);
 }
