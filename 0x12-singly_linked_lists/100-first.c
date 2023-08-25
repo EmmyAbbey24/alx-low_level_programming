ca010cfea96740005a71b8ccdfd8d5ea9f1b33e9
EmmyAbbey24 <princeemmy010@gmail.com>
2023-08-25 16:16:47 -0700

    commit string

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 
-// Declare the function prototype
+/* Declare the function prototype */
 void print_intro(void) __attribute__((constructor));
 
-// Function that prints an introduction
+/**
+ * print_intro - Prints an introduction
+ *
+ * Description: This function is executed automatically
+ * before the main function is called. It prints two lines.
+ */
 void print_intro(void)
 {
     printf("You're beat! and yet, you must allow,\n");
     printf("I bore my house upon my back!\n");
 }
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    // The main function is empty in this example
+    /* The main function is empty in this example */
     return 0;
 }
