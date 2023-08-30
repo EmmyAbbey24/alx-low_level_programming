0b3725e1850b56c0b242da84118ca473c5f25344
EmmyAbbey24 <princeemmy010@gmail.com>
2023-08-29 19:51:04 -0700

    new commit

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,35 +1,32 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - Frees a listint_t list, even if there's a loop.
+ * free_listint_safe - Frees a listint_t list even if there's a loop.
  * @h: A pointer to a pointer to the head of the list.
  *
  * Return: The size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
+listint_t *latest, *next;
 size_t count = 0;
-listint_t *latest, *temp;
 
-if (h == (listint_t **)NULL)
-return (count);
+if (h == NULL || *h == NULL)
+return (0);
 
-latestt = *h;
-
-while (latest != (listint_t *)NULL)
+latest = *h;
+while (latest != NULL)
 {
-temp = latest;
-latest = latest->next;
-free(temp);
+next = latest->next;
+free(latest);
 count++;
 
-// Break if loop detected
-if (temp->next >= temp)
-{
-*h = (listint_t *)NULL;
+if ((size_t)latest <= (size_t)next)
 break;
-}
+
+latest = next;
 }
 
+*h = NULL;
 return (count);
 }
