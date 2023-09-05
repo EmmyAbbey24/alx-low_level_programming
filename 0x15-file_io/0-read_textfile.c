7d9099b3df9529588bd0d9ee4cfb0661fae91608
EmmyAbbey24 <princeemmy010@gmail.com>
2023-09-04 19:35:56 -0700

    initial commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include "main.h"
 
 /**
  * read_textfile - reads a text file and prints the letters
- * @filename: text file being read
+ * @filename: filename.
  * @letters: numbers of letters printed.
  *
  * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-if (filename == NULL)
+int fd;
+ssize_t rd, wt;
+char *buf;
+
+if (!filename)
 return (0);
 
-int fd = open(filename, O_RDONLY);
+fd = open(filename, O_RDONLY);
+
 if (fd == -1)
 return (0);
 
-char *buffer = malloc(letters);
-if (buffer == NULL)
-{
-close(fd);
+buf = malloc(sizeof(char) * (letters));
+if (!buf)
 return (0);
-}
 
-ssize_t bytes_read = read(fd, buffer, letters);
-if (bytes_read == -1)
-bytes_read = 0;
+rd = read(fd, buf, letters);
+wt = write(STDOUT_FILENO, buf, nrd);
 
-ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-free(buffer);
 close(fd);
 
-return (bytes_read == bytes_written) ? bytes_read : (0);
+free(buf);
+
+return (wt);
 }
