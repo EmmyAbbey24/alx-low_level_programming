ca135e073d256031666e355675a017cc65edd7f8
EmmyAbbey24 <princeemmy010@gmail.com>
2023-09-04 20:14:35 -0700

    another commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,12 +6,11 @@
  * @filename: filename.
  * @letters: numbers of letters printed.
  *
- * Return: numbers of letters printed. It fails, returns 0.
+ * Return: numbers of letters printed. It fails, return 0.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
-{
 
+{
 int fd;
 ssize_t rd;
 ssize_t wt;
