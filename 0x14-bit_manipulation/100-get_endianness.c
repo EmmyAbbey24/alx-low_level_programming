691e2ead112c2d4d83754099912157962be47dc5
root <princeemmy010@gmail.com>
2023-09-28 15:37:55 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks if a machine is little or big endian
+ * Return: 0 for big, 1 for little
+ */
+int get_endianness(void)
+{
+unsigned int i_end = 1;
+char *c = (char *) &i_end;
+
+return (*c);
+}
+
+
+0–binary-to-unit.c
+
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+int i;
+unsigned int result = 0;
+
+if (!b)
+return (0);
+
+for (i = 0; b[i]; i++)
+{
+if (b[i] < '0' || b[i] > '1')
+return (0);
+result = 2 * result + (b[i] - '0');
+}
+
+return (result);
+}
