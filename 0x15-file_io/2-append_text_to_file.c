f34fb12a749eecbae7c36e2514d77d6536fbf4f0
EmmyAbbey24 <princecemmy010@gmail.com>
2023-10-03 18:35:48 -0700

    file commit

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "MAIN_H"
+
+/**
+ * append_text_to_file - Appends text to the end of a file.
+ * @file: Pointer to the file name.
+ * @text: The string to append.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int append_text_to_file(const char *file, char *text)
+{
+	int fd, wr, length = 0;
+
+	if (file == NULL)
+		return (-1);
+
+	if (text != NULL)
+	{
+		while (text[length])
+			length++;
+	}
+
+	fd = open(file, O_WRONLY | O_APPEND);
+	wr = write(fd, text, length);
+
+	if (fd == -1 || wr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
+
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,29 @@
+#include "MAIN_H"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *alloc_buffer(char *file);
+void close_fd(int fd);
+
+/**
+ * alloc_buffer - Allocate a 1024-byte buffer.
+ * @file: The associated file name.
+ *
+ * Return: Pointer to the newly-allocated buffer.
+ */
+char *alloc_buffer(char *file)
+{
+	char *buf;
+
+	buf = malloc(sizeof(char) * 1024);
+
+	if (buf == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buf);
+}
+
